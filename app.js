e85bd8d9422c25dbe08acbc4b0e9ada996bf6791
jinseongjoo <qpyou1234@naver.com>
2018-10-04 21:31:53 +0900

    day2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: "#app",
     data: {
-        title: "아, 비 오네… 빨래가 잘 안마르겠는걸…"
+        title: "안녕! Vuejs!"
+    },
+    methods:{
+        HowAreYou: function() {
+            return "기분이 어때?"
+        },
+        HowAreYou: function() {
+            return this.title
+        }
     }
 })
