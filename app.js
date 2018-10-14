97fb25491c4553ac2657e3eb83a93fab1c928cf6
jinseongjoo <qpyou1234@naver.com>
2018-10-14 16:51:41 +0900

    day12 가위바위보 게임 | 10. 리펙토링

--- app.js
+++ app.js
@@ -8,73 +8,53 @@ new Vue({
         lifeOfMe: 3,
         lifeOfCom: 3,
         isSelectable: true,
-        logs: []
+        logs: [],
+        selects: [
+            { name: "가위", value: "scissor" },
+            { name: "바위", value: "rock" },
+            { name: "보", value: "paper" }
+        ]
+    },
+    computed: {
+        myChoiceImg() {
+            return this.myChoice!==null ? `images/${this.myChoice}.jpg` : "images/question.jpg"
+        },
+        comChoiceImg() {
+            return this.comChoice!==null ? `images/${this.comChoice}.jpg` : "images/question.jpg"
+        },
+        leftLifeOfMe() {
+            return 3 - this.lifeOfMe
+        },
+        leftLifeOfCom() {
+            return 3 - this.lifeOfCom
+        }
     },
     watch: {
-        count: function (newVal) {
+        count(newVal) {
             if (newVal === 0) {
-                let number = Math.random()
-                if (number < 0.33) {
-                    this.comChoice = "scissor"
-                }
-                else if (number < 0.66) {
-                    this.comChoice = "rock"
-                }
-                else {
-                    this.comChoice = "paper"
-                }
-                // 가위바위보 승패 결정
-                if (this.myChoice === this.comChoice) this.winter = 'no one'
-                else if (this.myChoice === 'rock' && this.comChoice === 'scissor') this.winter = 'me'
-                else if (this.myChoice === 'scissor' && this.comChoice === 'paper') this.winter = 'me'
-                else if (this.myChoice === 'paper' && this.comChoice === 'rock') this.winter = 'me'
-                else if (this.myChoice === 'scissor' && this.comChoice === 'rock') this.winter = 'com'
-                else if (this.myChoice === 'paper' && this.comChoice === 'scissor') this.winter = 'com'
-                else if (this.myChoice === 'rock' && this.comChoice === 'paper') this.winter = 'com'
-                else this.winter = 'error'
-                // 몫 차감
-                if (this.winter === 'me') {
-                    this.lifeOfCom--
-                }
-                else if (this.winter === 'com') {
-                    this.lifeOfMe--
-                }
+                // 가위바위보를 선택하는 부분
+                this.selectOne()
+                // 가위바위보 승패 결정 & 몫 차감
+                this.whoIsWin()
+
+                // 게임 리셋
                 this.count = 3
-                // 버튼은 다시 보이게 됨
                 this.isSelectable = true
-                let log = {
-                    message: `You ${this.myChoice}, Computer ${this.comChoice}`,
-                    winner: this.winter
-                }
-                this.logs.unshift(log)
+
+                // 로그를 업데이트 하는 부분
+                this.updateLogs()
             }
         },
-        lifeOfMe: function (newVal) {
+        lifeOfMe(newVal) {
             if(newVal === 0) {
-                setTimeout(()=>{
-                    confirm("안타깝네요. 당신이 패배하였습니다.")
-                    this.lifeOfMe = 3
-                    this.lifeOfCom = 3
-                    this.myChoice = null
-                    this.comChoice = null
-                    this.winter = null
-                    this.count = 3
-                    this.logs = []
-                }, 500)
+                // 게임을 종료
+                this.endGame("안타깝네요. 당신이 패배하였습니다.")
             }
         },
-        lifeOfCom: function (newVal) {
+        lifeOfCom(newVal) {
             if(newVal === 0) {
-                setTimeout(()=>{
-                    confirm("축하드립니다. 당신이 승리하였습니다.")
-                    this.lifeOfMe = 3
-                    this.lifeOfCom = 3
-                    this.myChoice = null
-                    this.comChoice = null
-                    this.winter = null
-                    this.count = 3
-                    this.logs = []
-                }, 500)
+                // 게임을 종료
+                this.endGame("축하드립니다. 당신이 승리하였습니다.")
             }
         }
     },
@@ -92,6 +72,54 @@ new Vue({
                     }
                 }, 1000)
             }
+        },
+        selectOne() {
+            let number = Math.random()
+            if (number < 0.33) {
+                this.comChoice = "scissor"
+            }
+            else if (number < 0.66) {
+                this.comChoice = "rock"
+            }
+            else {
+                this.comChoice = "paper"
+            }
+        },
+        whoIsWin() {
+            if (this.myChoice === this.comChoice) this.winter = 'no one'
+            else if (this.myChoice === 'rock' && this.comChoice === 'scissor') this.winter = 'me'
+            else if (this.myChoice === 'scissor' && this.comChoice === 'paper') this.winter = 'me'
+            else if (this.myChoice === 'paper' && this.comChoice === 'rock') this.winter = 'me'
+            else if (this.myChoice === 'scissor' && this.comChoice === 'rock') this.winter = 'com'
+            else if (this.myChoice === 'paper' && this.comChoice === 'scissor') this.winter = 'com'
+            else if (this.myChoice === 'rock' && this.comChoice === 'paper') this.winter = 'com'
+            else this.winter = 'error'
+            // 몫 차감
+            if (this.winter === 'me') {
+                this.lifeOfCom--
+            }
+            else if (this.winter === 'com') {
+                this.lifeOfMe--
+            }
+        },
+        updateLogs() {
+            let log = {
+                message: `You ${this.myChoice}, Computer ${this.comChoice}`,
+                winner: this.winter
+            }
+            this.logs.unshift(log)
+        },
+        endGame(msg) {
+            setTimeout(()=>{
+                confirm(msg)
+                this.lifeOfMe = 3
+                this.lifeOfCom = 3
+                this.myChoice = null
+                this.comChoice = null
+                this.winter = null
+                this.count = 3
+                this.logs = []
+            }, 500)
         }
     }
 })
