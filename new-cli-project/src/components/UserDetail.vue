e49fd07e0ce6a90ea249b9ed5455843a7dac0ecd
jinseongjoo <qpyou1234@naver.com>
2018-10-21 15:20:23 +0900

    day19 props의 활용

--- new-cli-project/src/components/UserDetail.vue
+++ new-cli-project/src/components/UserDetail.vue
@@ -2,13 +2,31 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()">이름 변경</v-btn>
   </div>
 </template>
 <script>
   export default {
-      props: ["nameOfChild"],
+      props: {
+          nameOfChild: {
+              type: String,
+              default:"LEGO",
+              required: true
+          }
+      },
       data() {
           return {
+          }
+      },
+      computed: {
+          sayHello() {
+              return `${this.nameOfChild} 안녕하세요.`
+          }
+      },
+      methods: {
+          switchName () {
+              this.nameOfChild = '컴퓨터'
           }
       }
   }
