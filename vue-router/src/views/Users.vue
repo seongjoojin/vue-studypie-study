b5cb572f14bb4fc67ce42487c4a905b1053bdeb8
jinseongjoo <qpyou1234@naver.com>
2018-11-03 17:13:08 +0900

    Vue Router | VueJS 3 | Guard

--- vue-router/src/views/Users.vue
+++ vue-router/src/views/Users.vue
@@ -32,9 +32,19 @@
                 userId: null
             }
         },
+        beforeRouteEnter(to, from, next) {
+            console.log("before Enter")
+            next()
+        },
+        beforeRouteLeave(to, from, next) {
+            console.log("before Leave")
+            next()
+        },
         created() {
-            console.log("router", this.$router)
-            console.log("route", this.$route)
+            console.log("created")
+        },
+        destroyed() {
+            console.log("destroyed")
         }
     }
 </script>
