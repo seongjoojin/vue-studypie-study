6ee7ba68140d59e8721343e38047896c20fee323
jinseongjoo <qpyou1234@naver.com>
2018-11-14 23:04:12 +0900

    Vuex & Router 실습 예제 (1) | 로그인 상태 관리하기 | Vuex | Vue Router

---
+++ ex-project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<v-app id="inspire">
+		<v-navigation-drawer
+						v-model="drawer"
+						fixed
+						app
+		>
+			<v-list dense>
+				<v-list-tile router :to="{name: 'home'}">
+					<v-list-tile-action>
+						<v-icon>home</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>Home</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+				<v-list-tile router :to="{name: 'login'}">
+					<v-list-tile-action>
+						<v-icon>contact_mail</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>로그인</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-toolbar color="indigo" dark fixed app>
+			<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+			<v-toolbar-title>Application</v-toolbar-title>
+		</v-toolbar>
+		<v-content>
+			<router-view/>
+		</v-content>
+		<v-footer color="indigo" app>
+			<span class="white--text">&copy; 2017</span>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
---
+++ ex-project/src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+	<v-container fill-height style="max-width:450px;">
+		<v-layout align-center row wrap>
+			<v-flex xs12>
+					<v-card>
+						<v-toolbar flat>
+								<v-toolbar-title>로그인</v-toolbar-title>
+						</v-toolbar>
+						<div class="pa-3">
+							<v-text-field
+											v-model="email"
+											label="이메일을 입력하세요."
+							></v-text-field>
+							<v-text-field
+											v-model="password"
+											type="password"
+											label="패스워드를 입력하세요."
+							></v-text-field>
+						</div>
+					</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+	data() {
+  	return {
+  		email: null,
+			password: null
+		}
+	}
+};
+</script>
+
+<style scoped>
+</style>
